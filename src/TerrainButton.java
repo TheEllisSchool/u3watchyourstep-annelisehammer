f163bc8fcbb7e099fbe420613210503eb43180a5
annelisehammer <annelisehammer@172.16.21.61>
2019-12-16 11:05:47 -0500

    Hi

--- src/TerrainButton.java
+++ src/TerrainButton.java
@@ -1,4 +1,51 @@
+import java.awt.Color;
+import java.awt.Dimension;
 
-public class TerrainButton {
+import javax.swing.JButton;
 
+public class TerrainButton extends JButton{
+	private static final int SIZE = 50; 
+	
+	//attributes - position (row, col) , clicked? , Bomb? , surrounding?/next to bomb , flagged?
+	
+	private int row ;
+	private int col;
+	private boolean isBomb = false;
+	private boolean isClicked = false;
+	private int nextToBombs = 0; //make sure you recalculate this value once everything is created
+	private boolean isFlagged = false;
+	
+	//methods- getters/setters
+	public TerrainButton(int r, int c) {
+		row = r;
+		col = c;
+		
+		Dimension size = new Dimension (SIZE, SIZE);
+		setPreferredSize(size);
+	}
+
+	public int getRow () {
+		return row;
+	}
+	public int getCol() {
+		return col;
+	}
+	public void setBomb() {
+		isBomb = true;
+	}
+	public boolean hasBomb() {
+		return isBomb;
+	}
+	public boolean beenClicked() {
+		return isClicked;
+	}
+	public void click() { //probably will add to this once we know what happens when it's clicked
+		isClicked = true;
+		//System.out.println(row + " and " + col + " has been clicked!");
+		if (isBomb) {
+			setBackground(Color.RED);
+		}else {
+			setBackground(Color.BLACK);
+		}
+	}
 }
--- src/WatchYourStep.java
+++ src/WatchYourStep.java
@@ -1,34 +1,89 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.UIManager;
 
 public class WatchYourStep extends JFrame{
 	
-
+	
+	private static final int GRIDSIZE = 10;
+	//create a 2D array
+	private TerrainButton [] [] terrain = new TerrainButton [GRIDSIZE] [GRIDSIZE];
+	private static final int NUMBEROFBOMBS = 10;
 	public WatchYourStep(){	
 		
 		initGUI();
+		setBombs();
 		
-		setTitle("Game Window");
+		setTitle("Bombsweeper");
 		setSize(200, 100); //pixels
 		setResizable(true);
-		pack();
+		pack(); //minimum pixels needed
 		setLocationRelativeTo(null); //centers on screen, do this after packing but before visible
 
 			
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true); //to be able to see it
+		setDefaultCloseOperation(EXIT_ON_CLOSE); //the red x 
 
 	}
 	
-	public void initGUI(){
-		JLabel titleLabel = new JLabel("Watch Your Step");
-		add(titleLabel);
-		titleLabel.setHorizontalAlignment(JLabel.CENTER); //left or right
+	public void initGUI(){ //everything inside the window
+		Font titleFont = new Font ("Arial", Font.BOLD, 32);
+		JLabel titleLabel = new JLabel("Watch Your Step"); 
+		titleLabel.setFont(titleFont);
+		titleLabel.setHorizontalAlignment(JLabel.CENTER); //left or right, location
+		titleLabel.setForeground(Color.PINK);
+		titleLabel.setBackground(Color.BLACK);
+		titleLabel.setOpaque(true);
+		add(titleLabel, BorderLayout.PAGE_START); //you have to add everything (sout)
+		
+		JPanel centerPanel = new JPanel();
+		centerPanel.setLayout(new GridLayout(GRIDSIZE,GRIDSIZE));
+		add(centerPanel, BorderLayout.CENTER);
+		//TerrainButton testButton = new TerrainButton (0, 0);
+		//centerPanel.add(testButton);
+		for (int r = 0; r < GRIDSIZE; r++) {
+			for (int c = 0; c< GRIDSIZE; c++) {
+				terrain[r][c] = new TerrainButton (r,c);
+				terrain [r][c].addActionListener(new ActionListener() {
+					public void actionPerformed (ActionEvent e) {
+						TerrainButton button = (TerrainButton) e.getSource();
+						int row = button.getRow();
+						int col = button.getCol();
+						terrain[row][col].click();
+					}
+				});
+								
+				centerPanel.add(terrain[r][c]);
+			}
+		}	
+		
 	}
-
+	
+	public void setBombs() {
+		Random rand = new Random();
+		int pickRow;
+		int pickCol;
+		for (int i = 0; i < NUMBEROFBOMBS; i++) {
+			do { 
+				pickRow = rand.nextInt(GRIDSIZE);
+				pickCol = rand.nextInt(GRIDSIZE);
+			}while(terrain [pickRow][pickCol].hasBomb());
+			terrain[pickRow][pickCol].setBomb();
+		}
+	}
+	
+	
+//copy and paste this when needed for other codes
 	public static void main(String[] args) {
 		try {
             String className = UIManager.getCrossPlatformLookAndFeelClassName();
